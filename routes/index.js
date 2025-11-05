31cc9e2f1015a45a8fbbce2267ddf670623d0360
AlexTWilson95 <Alexander.wilson@ontariotechu.net>
2025-11-05 15:09:05 -0500

    routes index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res,) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/', function(req, res,) {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET projects page. */
+router.get('/', function(req, res,) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET contact page. */
+router.get('/', function(req, res,) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
