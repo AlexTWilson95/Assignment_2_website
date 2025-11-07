57b3e2a791e64e6dd1f2314cd01cbf8783ed99e1
AlexTWilson95 <Alexander.wilson@ontariotechu.net>
2025-11-07 12:14:17 -0500

    Third major push: all pages and formatting working

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 
 /* GET home page */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', activePage: "" });
 });
 
 /* GET about page */
 router.get('/about', function(req, res) {
-  res.render('about', { title: 'About Me' });
+  res.render('about', { title: 'About Me', activePage: "about" });
 });
 
 /* GET projects page */
 router.get('/projects', function(req, res) {
-  res.render('projects', { title: 'Projects' });
+  res.render('projects', { title: 'Projects', activePage: "projects" });
 });
 
 /* GET contact page */
 router.get('/contact', function(req, res) {
-  res.render('contact', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me', activePage: "contact" });
 });
 
 module.exports = router;
